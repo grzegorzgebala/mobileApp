17d2e8883b43fcbfc3591d3d7c153b82e3170406
Grzegorz <grzegorz.gebala@yahoo.com>
2019-12-07 23:37:29 +0100

    First site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as Background } from "./Background/1_loginPage/background.svg";
+import { ReactComponent as StatusBar } from "./Background/0_common/statusBar.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Background className="background" />
+      <StatusBar className="status" />
+      <div className="appInfo">
+        <div className="topic">YOUR ART MUSEUM</div>
+        <div className="desc">151 3rd St San Francisco, CA 94103</div>
+      </div>
+      <div className="logInFields">
+        <input></input>
+        <input></input>
+        <div className="forgotPass">Forgot your password?</div>
+      </div>
+      <div className="logInButton">
+        <button></button>
+        <div className="forgotPass">Don't have an account</div>
+      </div>
     </div>
   );
 }
